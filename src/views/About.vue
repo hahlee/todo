bf0bc1e29643bf02c465847af6988a40777e9bd7
hahlee <hahlee93@gmail.com>
2019-06-17 23:08:54 -0600

    add header to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,21 @@
 <template>
   <div class="about">
+    <Header />
     <h1>About</h1>
     <p>This is my Todo List app v1.0.0. It uses <a href="https://jsonplaceholder.typicode.com/" target="_blank">JSON Placeholder</a> as the API</p>
   </div>
 </template>
+
+<script>
+import Header from "@/components/layout/Header";
+export default {
+  name: "About",
+  components: {
+    Header
+  }
+}
+</script>
+
 
 <style lang="sass" scoped>
   @import '@/styles/global.sass'
